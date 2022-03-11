548de3a983a19c29ffe098e42dcc7b12fa11ae50
Ophélie <ophelie.laetitia.schauffler@gmail.com>
2022-03-11 11:26:31 +0100

    Composant Formulaire1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { FormulaireUtilisateurComponent } from './formulaire-utilisateur/formulaire-utilisateur.component';
 
 const routes: Routes = [
-  { path: 'app', component: AppComponent}
+  {
+    path: "Inscrire", component: FormulaireUtilisateurComponent
+  }
 ];
 
 @NgModule({
