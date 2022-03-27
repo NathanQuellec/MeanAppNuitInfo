8473263ad05ac5d7543fd25b136dcf79b0b037c4
Ophélie <ophelie.laetitia.schauffler@gmail.com>
2022-03-27 18:48:02 +0200

    [FRONT] Service User

--- client/src/app/formulaire-utilisateur/formulaire-utilisateur.component.ts
+++ client/src/app/formulaire-utilisateur/formulaire-utilisateur.component.ts
@@ -1,13 +1,9 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
 import { User } from '../user';
+import { UserService } from '../services/user.service';
 
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type' : 'application/x-www-form-urlencoded'})
-} 
 
 @Component({
   selector: 'app-formulaire-utilisateur',
@@ -22,7 +18,7 @@ export class FormulaireUtilisateurComponent implements OnInit {
 
   submitted = false;
 
-  constructor(private http: HttpClient, private route: Router){}
+  constructor(private http: HttpClient, private route: Router, private user: UserService){}
 
   onSubmit() { this.submitted = true; }
 
@@ -31,20 +27,14 @@ export class FormulaireUtilisateurComponent implements OnInit {
 
   infoPerso(utilisateurForm: any){
     let data = utilisateurForm.value
+
     console.log(utilisateurForm)
-      let name: string = data.name
-      let surname: string = data.surname
-      let age: number = data.age
+
+    let name: string = data.name;
+    let surname: string = data.surname;
+    let age: number = data.age;
     
-      const body = new HttpParams()
-      .append('name', name)
-      .append('surname', surname)
-      .append('age', age)
-      
-      console.log(body)
-      this.http.post(environment.apiUrl+"/users", body, httpOptions).subscribe(result => 
-      this.resultPost = result);
-      console.log(this.resultPost);
-      this.route.navigate(['/Accueil']);
+    this.user.informationsPerso(name, surname, age);
+    this.route.navigate(['/']);
   }
 }
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  
+  
+  body: String | any;
+  resultPost: String | any;
+
+  constructor(private http: HttpClient) { }
+
+  informationsPerso(name : string, surname : string, age: number){
+    const body = new HttpParams()
+      .append('name', name)
+      .append('surname', surname)
+      .append('age', age)
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type' : 'application/x-www-form-urlencoded'})
+    } 
+    
+    console.log(body)
+    this.http.post(environment.apiUrl+"/users", body, httpOptions).subscribe(result => 
+    this.resultPost = result);
+    console.log(this.resultPost);
+  }
+}
