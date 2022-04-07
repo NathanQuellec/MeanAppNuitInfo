ff94083b8b7e9f5a50742b2d72d0e6e781546229
Ophélie <ophelie.laetitia.schauffler@gmail.com>
2022-04-07 11:44:18 +0200

    [FRONT] Valeur form

---
+++ client/src/app/modules/resultatsMaladies/avc/avc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avc',
+  templateUrl: './avc.component.html',
+  styleUrls: ['./avc.component.css']
+})
+export class AvcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
