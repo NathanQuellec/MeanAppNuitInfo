72c8f40c161129517acf0a2b5243880b008cf081
Hariprasad Raypoulet <hari-prasad009@hotmail.fr>
2022-04-13 14:02:00 +0200

    [ADD] - Work on test unitaire

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 import { UserRoutes } from './user.routes.js';
+import { TestRoutes } from './test.routes.js';
 
 export const configureRoutes = app => {
     UserRoutes(app);
+    TestRoutes(app);
     return app;
 }
