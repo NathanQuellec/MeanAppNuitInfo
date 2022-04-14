ac1241ce302839cd1cd4ec1a2da0f559903a3acd
Ophélie <ophelie.laetitia.schauffler@gmail.com>
2022-04-14 13:59:35 +0200

    [FRONT] Form diabete + cardiaque

---
+++ client/src/app/user-diabete.ts
@@ -0,0 +1,26 @@
+export class UserDiabete {
+
+    public constructor(
+      public hypertension: number,
+      public cholesterol: number,
+      public cholCheck: number,
+      public bmi: number,
+      public smoke: number,
+      public stroke: number,
+      public heart: number,
+      public physActivity: number,
+      public fruits: number,
+      public veggies: number,
+      public alcohol: number,
+      public healthcare: number,
+      public noDocbcCost: number,
+      public genHlth:number,
+      public mentHlth: number,
+      public physHlth: number,
+      public diffWalk: number,
+      public genre: number,
+      public age: number,
+      public education: number,
+      public income: number
+    ) {  }
+  }
