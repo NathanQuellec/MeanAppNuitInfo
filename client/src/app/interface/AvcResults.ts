878f219a4f264068080654fa885b7c75beadbfc3
Nathan <nathan.quellec@gmail.com>
2022-04-14 16:35:30 +0200

    data viz - return only true prediction

--- client/src/app/interface/AvcResults.ts
+++ client/src/app/interface/AvcResults.ts
@@ -1,4 +1,5 @@
 export interface AvcResults {
     prediction: boolean;
     score: Number;
+    createdAt: string;
 }
--- client/src/app/modules/resultatsMaladies/avc/avc.component.ts
+++ client/src/app/modules/resultatsMaladies/avc/avc.component.ts
@@ -9,81 +9,78 @@ import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
   styleUrls: ['./avc.component.css'],
 })
 export class AvcComponent implements OnInit {
-
   ngOnInit(): void {
     this.getAVCResultsModelFromAPI();
     this.getAVCResultsModelHistoryFromAPI();
-    this.dataArray();
-    console.log("test")
-    console.log(this.dataset)
   }
-  
+
   constructor(private user: UserService) {}
 
-  displayResultsModel: string = '';
   avcResults: AvcResults | any;
-  avcResultsHistory: Array<AvcResults> | any;
   avcScore: Number = 0;
 
-
   public type: ChartType = 'bar';
-  
-  public barChartLabel: string [] = ['date1', 'date2', 'date3'];
 
-  public backgroundColor: string[] = [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)'
-      ];
+  public barChartLabel: string[] = [];
 
-  public borderColor: string [] = [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)'
-      ];
+  public backgroundColor: string[] = [];
 
-  public data: number [] = [0.8, 0.6, -0.9]
+  public borderColor: string[] = [];
+
+  public pointHoverBackgroundColor: string[] = [];
 
   public dataset: ChartData<'bar'> = {
-    labels : this.barChartLabel,
-      datasets: [{
-        label : 'risque',
-        data : this.data,
-        borderColor: this.borderColor,
-        backgroundColor: this.backgroundColor,
-        borderWidth: 1
-       }]
-    }
+    labels: [],
+    datasets: [
+      {
+        data: [],
+      },
+    ],
+  };
 
-    public barChartOptions: ChartConfiguration['options'] = {
-      responsive: true
-    };
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
 
   getAVCResultsModelFromAPI() {
     this.user.getAVCResultsModel().subscribe((result: AvcResults) => {
-      this.avcResults = result
-      this.avcScore = Number(result.score)*100; 
+      this.avcResults = result;
+      this.avcScore = Number(result.score) * 100;
       console.log(this.avcResults);
     });
   }
 
   getAVCResultsModelHistoryFromAPI() {
-    let history: Array<AvcResults>;
-    this.user.getAVCResultsModelHistory().subscribe((result: Array<AvcResults>) => {
-      this.avcResultsHistory = result;
-      console.log(this.avcResultsHistory);
-    });
-    console.log(this.avcResultsHistory);
+    let history: number[] = [];
+    this.user
+      .getAVCResultsModelHistory()
+      .subscribe((avcResultsHistory: Array<AvcResults>) => {
+        
+        avcResultsHistory.forEach((avc) => {
+          history.push(Number(avc.score));
+          if (avc.prediction) {
+            this.backgroundColor.push('rgba(255, 99, 132, 0.2)');
+            this.borderColor.push('rgba(255, 99, 132, 1)');
+          } else {
+            this.backgroundColor.push('rgba(54, 162, 235, 0.2)');
+            this.borderColor.push('rgba(54, 162, 235, 1)');
+          }
+          this.barChartLabel.push(avc.createdAt.slice(0,10));
+        });
+
+        console.log(history);
+        this.dataset = {
+          labels: this.barChartLabel,
+          datasets: [
+            {
+              label: 'risque',
+              data: history,
+              borderColor: this.borderColor,
+              backgroundColor: this.backgroundColor,
+              borderWidth: 1,
+            },
+          ],
+        };
+      });
   }
-
-  dataArray(){
-    let test: Array<number> = [];
-    this.avcResultsHistory.array.forEach((element: any) => {
-      console.log(`test ${element.score}`)
-      test.push(element.score);
-    });
-    console.log(test);
-    return test;
-  }
-
-
-
 }
