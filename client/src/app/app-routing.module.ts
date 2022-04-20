137b64117fec1a7d347b10f8f91abd504ec0449e
Ophélie <ophelie.laetitia.schauffler@gmail.com>
2022-04-20 11:37:40 +0200

    [FRONT] Routing + page avc ok

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { FormulaireUtilisateurComponent } from './formulaire-utilisateur/formulaire-utilisateur.component';
-import { FormulaireAvcComponent } from './formulaire-avc/formulaire-avc.component';
 import { AvcComponent } from './modules/resultatsMaladies/avc/avc.component';
 import { CardiaqueComponent } from './modules/resultatsMaladies/cardiaque/cardiaque.component';
-import { FormulaireCardiaqueComponent } from './formulaire-cardiaque/formulaire-cardiaque.component';
-import { FormulaireDiabeteComponent } from './formulaire-diabete/formulaire-diabete.component';
 import { DiabeteComponent } from './modules/resultatsMaladies/diabete/diabete.component';
 
 const routes: Routes = [
@@ -21,21 +18,11 @@ const routes: Routes = [
     path: "Profile", component: PageUtilisateurComponent
   },
   {
-    path: "Informations/AVC", component: FormulaireAvcComponent
-  },
-  {
     path: "Maladies/AVC", component: AvcComponent
   },
   {
     path: "Maladies/Cardiaque", component: CardiaqueComponent
-  },
-  {
-    path: "Informations/Cardiaque", component: FormulaireCardiaqueComponent
-  },
-  {
-    path: "Informations/Diabete", component: FormulaireDiabeteComponent
-  },
-  {
+  },{
     path: "Maladies/Diabete", component: DiabeteComponent
   }
 ];
